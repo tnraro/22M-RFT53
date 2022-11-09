0fe829070911ad0b14bd60ba5fa509d68c3d2a57
tnRaro <deokji53@gamil.com>
2022-11-09 16:06:40 +0900

    build: add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AnalyticsWrapper } from './components/Analytics'
 import './globals.css'
 
 export default function RootLayout({
@@ -9,6 +10,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="p-6 max-w-md mx-auto bg-white rounded-md select-none">
         {children}
+        <AnalyticsWrapper />
       </body>
     </html>
   )
